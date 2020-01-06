f67ba936cecf3db9014cdf43d2765fae9457ab90
tezw21 <45884995+tezw21@users.noreply.github.com>
2020-01-06 22:19:03 +1300

    Bgs tally v1.1

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 A simple plugin for EDMC to count BGS work
 
 # Installation
-Download the [latest release](https://github.com/tezw21/BGS-Tally/releases/latest) of BGS Tally
+Download the [latest release](https://github.com/tezw21/BGS-Tally/releases/tag/v1.1) of BGS Tally
  - In EDMC, on the Plugins settings tab press the “Open” button. This reveals the plugins folder where this app looks for plugins.
  - Open the .zip archive that you downloaded and move the folder contained inside into the plugins folder.
 
 You will need to re-start EDMC for it to notice the new plugin.
 
 # Usage
-Enter the Faction name that you want to monitor in the *BGS Tally* tab in *Settings*. The name is not case sensitive. Once set the Faction is persistent until changed by the user
+Enter the Faction and system name that you want to monitor in the *BGS Tally* tab in *Settings*. The names are not case sensitive. Once set the Faction and System is persistent until changed by the user. It is up to the user to ensure that the Faction is present in the selected System and spelling is correct. Ctrl-v can be used to paste a name cut from 3rd party tools. 
+Once the Faction and System has been set the user needs to dock or launch and redock in the System to initalise the mission counter system. This only needs to be done once after they have been changed
 
 Only four positive actions are counted at the moment. Mission inf + , Total trade profit sold to Faction controlled station, Cartographic data sold to Faction controlled station, Bounties issued by named Faction. These total during the session and reset on restart of EDMC
 
-The data is not currently stored anywhere, including localy. If you need to total across sessions or keep a log of work done this needs to be done manually.
+The data is now saved locally. The counters can be reset by using the Counter Reset button. This could be used at each tick.
--- load.py
+++ load.py
@@ -2,11 +2,13 @@ import Tkinter as tk
 import myNotebook as nb
 import sys
 import ttk
+import json
+import requests
 from config import config
 from theme import theme
 
 this = sys.modules[__name__]	# For holding module globals
-this.FactionName =""
+
 this.MissionPoints =0
 this.TradeProfit =0
 this.BountiesCollected =0
@@ -17,11 +19,13 @@ def plugin_prefs(parent, cmdr, is_beta):
    """
    Return a TK Frame for adding to the EDMC settings dialog.
    """
-  
-   frame = nb.Frame(parent)
-   nb.Label(frame, text="Faction name to monitor").grid(column=0, sticky=tk.W)
-   factionname = nb.Entry(frame, textvariable =this.FactionName ,width=40).grid(column=0, sticky=tk.W)
    
+   frame = nb.Frame(parent)
+   nb.Label(frame, text="Faction to monitor").grid(column=0, sticky=tk.W)
+   factionname = nb.Entry(frame, textvariable = this.FactionName ,width=40).grid(column=0, sticky=tk.W)
+   systemlabel = nb.Label(frame, text="System to monitor").grid(column=0, sticky=tk.W)
+   systemname = nb.Entry(frame, textvariable =this.SystemName, width=40).grid(column=0, sticky=tk.W)
+   Reset = nb.Button(frame, text="Reset Counter").place(x=0 , y=290)
    return frame
 
 
@@ -29,25 +33,33 @@ def prefs_changed(cmdr, is_beta):
    """
    Save settings.
    """
-   config.set('XFactionName', this.FactionName.get())	# Store new value in config
    this.factionlabel2["text"] = this.FactionName.get()
-   this.MissionPoints =0
-   this.TradeProfit =0
-   this.BountiesCollected =0
-   this.CartDataSold=0
-   this.missioninf2["text"] = this.MissionPoints
-   this.tradeprofit2["text"] = this.TradeProfit
-   this.bountiescollected2["text"] = this.BountiesCollected
-   this.cartdata2["text"] = this.CartDataSold
-  
+   this.systemlabel2["text"] = this.SystemName.get()
+   this.MissionPoints.set(0)
+   this.TradeProfit.set(0)
+   this.BountiesCollected.set(0)
+   this.CartDataSold.set(0)
+   this.missioninf2["text"] = this.MissionPoints.get()
+   this.tradeprofit2["text"] = this.TradeProfit.get()
+   this.bountiescollected2["text"] = this.BountiesCollected.get()
+   this.cartdata2["text"] = this.CartDataSold.get()
+   
+     
    
 def plugin_start(plugin_dir):
    """
    Load this plugin into EDMC
    """
-   this.FactionName = tk.StringVar(value =config.get("XFactionName")) #load faction name
-   this.StationFaction = tk.StringVar(value =config.get("XStataionFaction")) #load faction name
-      
+   this.FactionName = tk.StringVar(value =config.get("XFactionName")) #load monitored faction name
+   this.StationFaction = tk.StringVar(value =config.get("XStataionFaction")) #load station controlling faction name
+   this.SystemName = tk.StringVar(value =config.get("XSystemName")) # load monitored system name
+   this.SystemAddress = tk.StringVar(value =config.get("XSystemAddress")) # load system address
+   this.MissionPoints = tk.IntVar(value=config.getint("XMissionPoints"))
+   this.TradeProfit = tk.IntVar(value=config.getint("XTradeProfit"))
+   this.BountiesCollected = tk.IntVar(value = config.getint("XBountiesCollected"))
+   this.CartDataSold = tk.IntVar(value = config.getint("XCartDataSold"))
+   
+
    return "BGS Tally"
 
 
@@ -55,7 +67,16 @@ def plugin_stop():
     """
     EDMC is closing
     """
-    print "Farewell cruel world!"
+    
+    config.set('XFactionName', this.FactionName.get())
+    config.set('XStataionFaction', this.StationFaction.get())
+    config.set('XSystemName', this.SystemName.get())
+    config.set('XSystemAddress', this.SystemAddress.get())
+    config.set('XMissionPoints', this.MissionPoints.get())
+    config.set('XTradeProfit', this.TradeProfit.get())
+    config.set('XBountiesCollected', this.BountiesCollected.get())
+    config.set('XCartDataSold', this.CartDataSold.get())
+    print ("Farewell cruel world!")
 
 def plugin_app(parent):
     """
@@ -69,66 +90,76 @@ def plugin_app(parent):
     factionlabel.grid(row=1, column=0, sticky=tk.W)
     this.factionlabel2 = tk.Label(this.frame, text = this.FactionName.get())
     this.factionlabel2.grid(row=1, column=1, sticky=tk.W)
+    systemlabel = tk.Label(this.frame, text="System:")
+    systemlabel.grid(row=2, column=0, sticky=tk.W)
+    this.systemlabel2 = tk.Label(this.frame, text = this.SystemName.get())
+    this.systemlabel2.grid(row=2, column=1, sticky=tk.W)
     missioninf = tk.Label(this.frame, text="Mission Points:")
-    missioninf.grid(row=2, column=0, sticky=tk.W)
-    this.missioninf2 = tk.Label(this.frame, text =this.MissionPoints)
-    this.missioninf2.grid(row=2, column=1,sticky=tk.W)
+    missioninf.grid(row=3, column=0, sticky=tk.W)
+    this.missioninf2 = tk.Label(this.frame, text =this.MissionPoints.get())
+    this.missioninf2.grid(row=3, column=1,sticky=tk.W)
     tradeprofit = tk.Label(this.frame, text="Trade Profit:")
-    tradeprofit.grid(row=3, column=0, sticky=tk.W)
-    this.tradeprofit2 = tk.Label(this.frame, text =this.TradeProfit)
-    this.tradeprofit2.grid(row=3, column=1, sticky=tk.W)
+    tradeprofit.grid(row=4, column=0, sticky=tk.W)
+    this.tradeprofit2 = tk.Label(this.frame, text =format(this.TradeProfit.get(), ',d'))
+    this.tradeprofit2.grid(row=4, column=1, sticky=tk.W)
     bountiescollected = tk.Label(this.frame, text="Bounties Collected:")
-    bountiescollected.grid(row=4, column=0, sticky=tk.W)
-    this.bountiescollected2 = tk.Label(this.frame, text =this.BountiesCollected)
-    this.bountiescollected2.grid(row=4,column=1, sticky=tk.W)
+    bountiescollected.grid(row=5, column=0, sticky=tk.W)
+    this.bountiescollected2 = tk.Label(this.frame, text =format(this.BountiesCollected.get(), ',d'))
+    this.bountiescollected2.grid(row=5,column=1, sticky=tk.W)
     cartdata = tk.Label(this.frame, text="Cartographic Data:")
-    cartdata.grid(row=5, column=0, sticky=tk.W)
-    this.cartdata2 =tk.Label(this.frame, text =this.CartDataSold)
-    this.cartdata2.grid(row=5, column=1, sticky=tk.W)
+    cartdata.grid(row=6, column=0, sticky=tk.W)
+    this.cartdata2 =tk.Label(this.frame, text =format(this.CartDataSold.get(), ',d'))
+    this.cartdata2.grid(row=6, column=1, sticky=tk.W)
    
             
     return this.frame
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-   if entry['event'] == 'Docked':   # set station name
+  
+   if entry['event'] == 'Docked':   # set controlling faction name
       sf = entry['StationFaction']
-      config.set('XStationFaction', sf)	# Store new value in config
-      this.StationFaction = sf['Name']
+      sa = entry['SystemAddress']
+      if system.lower() == this.SystemName.get().lower(): #set system address if system monitored
+         this.SystemAddress.set(sa)
          
+      this.StationFaction.set(sf['Name']) #set controlling faction name
+      
+      
    if entry['event'] =='MissionCompleted': # get mission influence value
       fe = entry['FactionEffects']
+      
       for i in fe:
          fe3 = i['Faction']
          fe4 = i['Influence']
+         print (fe3)
          for x in fe4:
             fe5 = x['Influence']
+            fe6 = str(x['SystemAddress'])
+
             inf = len(x['Influence'])
-            if fe3.lower() == this.FactionName.get().lower():
-               this.MissionPoints +=inf
-               this.missioninf2["text"] = this.MissionPoints
+            if fe3.lower() == this.FactionName.get().lower()and fe6 == this.SystemAddress.get():
+               this.MissionPoints.set(this.MissionPoints.get() + inf)
+               this.missioninf2["text"] = this.MissionPoints.get()
                
    if entry['event'] =='SellExplorationData' or entry['event'] == "MultiSellExplorationData": # get carto data value
-      print (entry)
-      if this.StationFaction.lower() == this.FactionName.get().lower():
-         this.CartDataSold += entry['TotalEarnings']
-         this.cartdata2["text"] = format(this.CartDataSold ,',d')
+      if this.StationFaction.get().lower() == this.FactionName.get().lower() and this.SystemName.get().lower() == system.lower():
+         this.CartDataSold.set(this.CartDataSold.get() + entry['TotalEarnings'])
+         this.cartdata2["text"] = format(this.CartDataSold.get() ,',d')
 
    if entry['event'] =='RedeemVoucher' and entry['Type'] == 'bounty': # bounties collected
       for z in entry['Factions']:
-         if z['Faction'].lower() == this.FactionName.get().lower():
-            this.BountiesCollected += z['Amount']
-
-
-      this.bountiescollected2['text'] = format(this.BountiesCollected ,',d')
+         if z['Faction'].lower() == this.FactionName.get().lower() and this.SystemName.get().lower() == system.lower():
+            this.BountiesCollected.set(this.BountiesCollected.get + z['Amount'])
+            this.bountiescollected2['text'] = format(this.BountiesCollected.get() ,',d')
 
       
 
    if entry['event'] == 'MarketSell': # Trade Profit
-      if this.StationFaction.lower() == this.FactionName.get().lower():
+      if this.StationFaction.get().lower() == this.FactionName.get().lower() and this.SystemName.get().lower() == system.lower():
          cost = entry['Count'] * entry['AvgPricePaid']
          profit = entry['TotalSale'] - cost
-         this.TradeProfit += profit
-         this.tradeprofit2['text'] = format(this.TradeProfit, ',d')
+         this.TradeProfit.set(this.TradeProfit.get() + profit)
+         this.tradeprofit2['text'] = format(this.TradeProfit.get(), ',d')
          
          
          
